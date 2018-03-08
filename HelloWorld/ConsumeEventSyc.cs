4f7dc8ff44ea3597fec6fd734e4a9645e3838e1b
irwinindy <irwinindy@users.noreply.github.com>
2018-03-08 18:39:17 -0500

    Add files via upload

---
+++ HelloWorld/ConsumeEventSyc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;  
+using System.Collections.Generic;  
+using System.Linq;  
+using System.Net;  
+using System.Text;
+using HelloWorldApi.Models;
+ 
+namespace HelloWorld
+{
+    class ConsumeEventSyc
+    {
+        public void GetAllEventData() //Get All Events Records  
+        {  
+            using(var client = new WebClient()) //WebClient  
+            {  
+                client.Headers.Add("Content-Type:application/json"); //Content-Type  
+                client.Headers.Add("Accept:application/json");
+                var result = client.DownloadString("http://localhost:51642/api/values"); //URI
+  
+                Console.Write(Environment.NewLine + result);
+                Console.Read();
+            }  
+        }  
+    }
+}
